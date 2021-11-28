067e6f524d81a6fd054edb6e0e82d352af0ae7d2
xdossantos <xola.dossantos@xdode.com>
2021-11-28 19:16:47 +0200

    Create Data Models and Db context

---
+++ HotelListing/Data/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.Data
+{
+    public class Country
+    {
+        // This is an entity 
+        // The table in our db will eb countries ( plural of this class model 
+    }
+}
---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext 
+    {
+        // This class represetnts the bridge betwween our defined class and the actual database 
+    }
+}
+
